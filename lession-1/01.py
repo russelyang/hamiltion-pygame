39dad022e271dc1bd5ecc8182023ccf6934ebe33
russelyang <russelyang@gmail.com>
2023-10-16 08:49:06 -0700

    doc: work on the document (#1)

--- lession-1/01.py
+++ lession-1/01.py
@@ -3,14 +3,14 @@ import pygame
 # initilize the game
 pygame.init()
 
+# define constants
 WINDOW_WIDTH=600
 WINDOW_HEIGHT=300
 
+# set display surface
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
 pygame.display.set_caption('hello world')
-
-#display_surface.fill((255,0,0), (0,0,200,200))
+display_surface.fill((255,0,0), (0,0,200,200))
 
 # main game loop
 running = True
@@ -20,4 +20,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     pygame.display.flip()
+
+# clean up and quit
 pygame.quit()
---
+++ lession-1/README.md
@@ -0,0 +1,18 @@
+# python fondamental
+
+# pygame
+
+https://www.pygame.org/docs/
+
+## Exercise
+
+1. Use the "print" function to print the line "Hello, World!".
+2. Use the "print" function to print out `1 plus 1 equal 2`, 2 is a result from `1+1`, not just print the answer
+
+## Homework
+
+follow the statements in `1.py`, and type it in your python program, you can use any file name.
+
+# useful links
+
+1. https://www.w3schools.com/python/default.asp
