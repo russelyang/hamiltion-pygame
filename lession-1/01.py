19ee8d91a219d3c87cf56e59146a35717c1c1fd3
russelyang <russelyang@gmail.com>
2023-10-26 09:08:17 -0700

    chore: finish week 1 (#2)

--- lession-1/01.py
+++ lession-1/01.py
@@ -4,6 +4,19 @@ import pygame
 pygame.init()
 
 # define constants
+<<<<<<< HEAD
+WINDOW_WIDTH = 600  # constant
+WINDOW_HEIGHT = 300
+
+RED = (255, 0, 0)
+
+
+# RGB A
+# set display surface
+display_surface = pygame.display.set_mode((600, 300))
+pygame.display.set_caption("hello world")
+display_surface.fill(RED, (0, 0, 200, 200))
+=======
 WINDOW_WIDTH=600
 WINDOW_HEIGHT=300
 
@@ -11,15 +24,22 @@ WINDOW_HEIGHT=300
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('hello world')
 display_surface.fill((255,0,0), (0,0,200,200))
+>>>>>>> main
 
+# boolean, a built in call bool
 # main game loop
-running = True
+running = True  # False
+
 while running:
     for event in pygame.event.get():
         print(event)
-        if event.type == pygame.QUIT:
+        if bool(event.type == pygame.QUIT):
             running = False
     pygame.display.flip()
 
 # clean up and quit
+<<<<<<< HEAD
 pygame.quit()
+=======
+pygame.quit()
+>>>>>>> main
