f3a5b53b9d07d7af14c1f91d024ccd98c9329390
russelyang <russelyang@gmail.com>
2023-10-30 09:17:03 -0700

    Week2 (#4)

--- lesson-2/02.py
+++ lesson-2/02.py
@@ -3,12 +3,20 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((960, 720))
 clock = pygame.time.Clock()
+
+backdrop = pygame.transform.scale(
+    pygame.image.load("./lession-2/Boardwalk.png").convert(), ((960, 720))
+)
+backdrop_rect = backdrop.get_rect()
+backdrop_rect.center = (480, 360)
+
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos = pygame.Vector2(screen.get_width() // 2, screen.get_height() / 2)
+
 
 while running:
     # poll for events
@@ -18,9 +26,11 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    # screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    # pygame.draw.circle(screen, "red", player_pos, 40)
+
+    screen.blit(backdrop, backdrop_rect)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -32,12 +42,14 @@ while running:
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
 
+    # print(dt * 300)
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
-    dt = clock.tick(60) / 1000
+
 
 pygame.quit()
---
+++ lesson-2/work_bench.py
@@ -0,0 +1,11 @@
+# TUPLE
+t = ("a", "b", "c")
+
+exist = "d" in t
+
+# CRUD
+
+# LIST / ARRAY
+list = ["a", "b", "c"]
+"a" in list
+# len(), index()
