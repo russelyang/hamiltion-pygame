58f0c61ce64ed5c233344310c7602111b80599cd
Russel Yang <Russel.Yang@take2games.com>
2023-11-06 18:41:48 -0800

    fix: add homework readme for week 4

---
+++ lesson-4/04.py
@@ -0,0 +1,32 @@
+import pygame
+
+pygame.init()
+
+# define game windows width hight
+WINDOW_WIDTH = 480
+WINDOW_HEIGHT = 360
+
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+ball = pygame.image.load("ball.png").convert_alpha()
+ball = pygame.transform.scale(ball, [48, 48])
+ball_rect = ball.get_rect()
+ball_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+speed = [2, 2]
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    ball_rect = ball_rect.move(speed)
+    screen.blit(ball, ball_rect)
+    if ball_rect.left < 0 or ball_rect.right > WINDOW_WIDTH:
+        speed[0] = -speed[0]
+    if ball_rect.top < 0 or ball_rect.bottom > WINDOW_HEIGHT:
+        speed[1] = -speed[1]
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
---
+++ lesson-4/readme.md
@@ -0,0 +1,20 @@
+# Summary
+
+## introduce the folder and file
+
+file and folder are used for organize the data in the computer system. please read this article for more information
+
+https://edu.gcfglobal.org/en/windowsbasics/working-with-files/1/
+
+folder and file are very important for us, please grasp the key knowledge.
+
+## Move the ball
+
+we code our program line by line in the class, all students are able to show the ball in the pygame display surface.
+
+## Homework
+
+1. shrink the ball size to make it look goods in the window
+2. let the ball bounce in the window.
+
+lesson-4/ball.py should work without any issue. please try to find the code and put it in your program.
